738abeec6176ba925b97dfddb9ee6b9e21b83ae2
Mohamed Ihsan Izwer <ihsanizwer@yahoo.com>
2022-02-25 18:34:36 +0530

    Changed connection string to suit linux

--- src/DevOpsChallenge.SalesApi.Database/DatabaseContextDesignTimeFactory.cs
+++ src/DevOpsChallenge.SalesApi.Database/DatabaseContextDesignTimeFactory.cs
@@ -12,7 +12,7 @@ namespace DevOpsChallenge.SalesApi.Database
         /// <inheritdoc />
         public DatabaseContext CreateDbContext(string[] args)
         {
-            string connectionString = Environment.GetEnvironmentVariable("CONNECTIONSTRINGS__DATABASE") ?? @"Server=(localdb)\mssqllocaldb;Database=DevOpsChallenge.SalesApi;Trusted_Connection=True;ConnectRetryCount=0";
+            string connectionString = Environment.GetEnvironmentVariable("CONNECTIONSTRINGS__DATABASE") ?? @"Server=localhost;Database=DevOpsChallenge.SalesApi;User Id=sa;Password=Rand0mP4$s11;Trusted_Connection=True;ConnectRetryCount=0";
 
             DbContextOptionsBuilder<DatabaseContext> optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>()
                 .UseSqlServer(connectionString);
