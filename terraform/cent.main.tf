c88ad669312dedd6352be3a9c5c76fb675674982
Mohamed Ihsan Izwer <ihsanizwer@yahoo.com>
2022-02-28 07:48:20 +0530

    Update cent.main.tf

--- terraform/cent.main.tf
+++ terraform/cent.main.tf
@@ -91,7 +91,7 @@ resource "azurerm_network_security_group" "devops_nsg" {
   location            = azurerm_resource_group.devops_rg.location
   resource_group_name = azurerm_resource_group.devops_rg.name
 
-  security_rule {
+  security_rule {                   
     name                       = "HTTP"
     priority                   = 400
     direction                  = "Inbound"
@@ -101,7 +101,19 @@ resource "azurerm_network_security_group" "devops_nsg" {
     destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
-  }
+  }                                 
+                                    
+    security_rule {                 
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }   
 }
 
 resource "azurerm_subnet_network_security_group_association" "devops-asc" {
